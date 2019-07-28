d04ff66301a2042647f11300b82e6715b37abf8a
Ro Han <rohank@valuefy.com>
2019-07-28 14:59:10 +0700

    code written

--- contracts/kyc.sol
+++ contracts/kyc.sol
@@ -117,7 +117,81 @@ contract kyc {
     /****
     * Bank/Blockchain Interface
     **/
+    // Add Request
+    function addRequest(string calldata _userName) external returns (bool) {
+        require(banks[msg.sender].ethAddress != address(0), "Bank not registered.");
+        require(banks[msg.sender].isAllowed == true, "Bank is Disabled from doing kyc.");
+        require(kycRequestMapping[msg.sender][_userName] == 0, "This user already has a KYC done by this bank.");
 
+        // Save the timestamp for this KYC request.
+        kycRequestMapping[msg.sender][_userName] = now;
+        return true;
+    }
+
+    //Add Customer
+    function addCustomer(string calldata _userName, string calldata _customerData) external returns (uint8) {
+        require(bytes(customers[_userName].userName).length == 0, "Customer already exists in system!");
+        customers[_userName].userName = _userName;
+        customers[_userName].customerData = _customerData;
+        customers[_userName].firstBank = msg.sender;
+        customerNames.push(_userName);
+        return 1;
+    }
+
+    //View Customer
+    function viewCustomer(string calldata _userName) external view returns (string memory, string memory, address) {
+        require(bytes(customers[_userName].userName).length != 0, "Customer not found!");
+        return (customers[_userName].userName, customers[_userName].customerData, customers[_userName].firstBank);
+    }
+
+    // Update Custoemr Data
+    function updateCustomerData(string calldata _userName, string calldata _customerData) external returns (uint8) {
+        require(bytes(customers[_userName].userName).length != 0, "Customer not found!");
+        customers[_userName].customerData = _customerData;
+        return 1;
+    }
+
+    // Delete Customer
+    function removeCustomer(string calldata _userName) external returns (uint8) {
+        require(bytes(customers[_userName].userName).length != 0, "Customer not found!");
+        delete customers[_userName];
+        return 1;
+    }
+
+    // Get All KYC Requests
+    function getKYCRequests() external view returns (uint256[] memory) {
+        uint256[] memory result;
+        // Loop through all users every added by any bank
+        for (uint8 i = 0; i < customerNames.length; i++) {
+            // If there is a timestamp for this user's KYC request, add it to the result array
+            if (kycRequestMapping[msg.sender][customers[customerNames[i]].userName] > 0) {
+                result[result.length - 1] = kycRequestMapping[msg.sender][customers[customerNames[i]].userName];
+            }
+        }
+        return result;
+    }
+
+    // Rate Customer
+    function rateCustomer(string calldata _userName) external returns (uint8) {
+        require(bytes(customers[_userName].userName).length != 0, "Customer not found!");
+        customers[_userName].rating++;
+        return 1;
+    }
+
+    // UpVote Customer
+    function upvoteCustomer(string calldata _userName) external returns (uint8) {
+        require(bytes(customers[_userName].userName).length != 0, "Customer not found!");
+        customers[_userName].upvotes++;
+        return 1;
+    }
+
+    // Rate Other Bank
+    function rateBank(address _ethAddress) external returns (uint8) {
+        require(banks[_ethAddress].ethAddress != address(0), "Bank is not registered!");
+        require(banks[_ethAddress].ethAddress != msg.sender, "You cannot rate yourself!");
+        banks[_ethAddress].rating++;
+        return 1;
+    }
 
     /**
      * Modifier to check if the message caller is an admin
